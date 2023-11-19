13512fa02eaf205b17a6a253da8f3a446fe31bfc
Denis Palnitsky <palnitsky@gmail.com>
2023-11-19 22:01:04 +0100

    Upgrade to .net 8

--- Startup.cs
+++ Startup.cs
@@ -34,13 +34,10 @@ namespace aspnetapp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
+          
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();                
             }
             else
             {
